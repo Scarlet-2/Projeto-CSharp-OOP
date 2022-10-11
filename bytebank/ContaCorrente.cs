1c041b5748c98d4a950a9bfec777e054c85e99c1
Matheus Silva <99970715+Scarlet-2@users.noreply.github.com>
2022-10-11 18:56:43 -0300

    Projeto Principal (Inicio)

---
+++ bytebank/ContaCorrente.cs
@@ -0,0 +1,13 @@
+
+using System;
+
+namespace bytebank
+{
+    public class ContaCorrente
+    {
+        public int numero_agencia;
+        public string conta;
+        public string titular;
+        public double saldo;
+    }
+}
