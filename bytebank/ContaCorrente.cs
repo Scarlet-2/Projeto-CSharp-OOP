9e47b1d9632d092a5aaa681764b484e3cdd909fb
Matheus Silva <99970715+Scarlet-2@users.noreply.github.com>
2022-10-11 19:46:00 -0300

    Sexta Aula - Terminada e criado os primeiros metodos

--- bytebank/ContaCorrente.cs
+++ bytebank/ContaCorrente.cs
@@ -1,13 +1,31 @@
 
 using System;
 
-namespace bytebank
+namespace bytebank;
+
+public class ContaCorrente
 {
-    public class ContaCorrente
-    {
-        public int numero_agencia;
-        public string conta;
-        public string titular;
-        public double saldo;
+    public int numero_agencia;
+    public string conta;
+    public string titular;
+    public double saldo;
+
+    // Depositar
+    public void Depositar(double valor) {
+        this.saldo += valor;
+    }
+
+    public void Sacar(double valor) {
+
+        if(valor >= this.saldo) {
+            System.Console.WriteLine("Você esta sacando mais que pode meu rapaz pode não.");
+        
+        } else {
+            this.saldo -= valor;
+
+        }
+
     }
 }
+
+
--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -3,9 +3,22 @@
 
 using bytebank;
 
-ContaCorrente matheus = new ContaCorrente();
-matheus.titular = "Matheus";
-matheus.numero_agencia = 22;
-matheus.saldo = 20000;
+ContaCorrente contaDoMatheus = new ContaCorrente();
+contaDoMatheus.titular = "Matheus";
+contaDoMatheus.numero_agencia = 22;
+contaDoMatheus.saldo = 20000;
+contaDoMatheus.conta = 228.ToString();
 
-Console.WriteLine("O saldo da minha conta corrente é: " + matheus.saldo);
+// Usando deposito
+contaDoMatheus.Depositar(2000); // Sucesso!!
+System.Console.WriteLine($"O meu saldo atual é: {contaDoMatheus.saldo}");
+
+// Sacar
+contaDoMatheus.Sacar(300);
+System.Console.WriteLine($"Eu saquei 300 Reais agora meu saldo é: {contaDoMatheus.saldo}");
+
+System.Console.WriteLine($"Titular da conta: {contaDoMatheus.titular}");
+System.Console.WriteLine($"Numero de Agência: {contaDoMatheus.numero_agencia}");
+System.Console.WriteLine($"Saldo da conta: {contaDoMatheus.saldo}");
+System.Console.WriteLine($"Numero da conta: {contaDoMatheus.conta}");
+
