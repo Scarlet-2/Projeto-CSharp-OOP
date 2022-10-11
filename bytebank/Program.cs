753629aadea55da8373678ec88a54bdb276f344a
Matheus Silva <99970715+Scarlet-2@users.noreply.github.com>
2022-10-11 19:08:54 -0300

    O resto

--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -1,2 +1,11 @@
 ﻿
-Console.WriteLine("Hello, World!");
+// Mudando conteudo de uma classe
+
+using bytebank;
+
+ContaCorrente matheus = new ContaCorrente();
+matheus.titular = "Matheus";
+matheus.numero_agencia = 22;
+matheus.saldo = 20000;
+
+Console.WriteLine("O saldo da minha conta corrente é: " + matheus.saldo);
